1594d4121fae85e2804591f28cc3e46a8749b451
Alex So <alex.so@icetrix.com>
2026-09-13 16:10:25 +0200

    chore: update ticker-vim to v5.4.0

--- Formula/ticker-vim.rb
+++ Formula/ticker-vim.rb
@@ -1,16 +1,16 @@
 class TickerVim < Formula
   desc "Terminal stock ticker with Vim navigation and fuzzy filtering"
   homepage "https://github.com/alexso/ticker-vim"
-  version "5.3.3"
+  version "5.4.0"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alexso/ticker-vim/releases/download/v5.3.3/ticker-vim_5.3.3_darwin_arm64.tar.gz"
-      sha256 "f0f7233bdf11eed2051716280f3b29eab2a39782e2d06cbd60ac42b098adfae4"
+      url "https://github.com/alexso/ticker-vim/releases/download/v5.4.0/ticker-vim_5.4.0_darwin_arm64.tar.gz"
+      sha256 "9e2afdf2499118362dab0b88bb8386af78c6d0b302417d83d2f4d9aa844f386b"
     else
-      url "https://github.com/alexso/ticker-vim/releases/download/v5.3.3/ticker-vim_5.3.3_darwin_amd64.tar.gz"
-      sha256 "355a0faffb2a9b1882facb2266d3d1475c613d4d53b03182bd17920d786a920f"
+      url "https://github.com/alexso/ticker-vim/releases/download/v5.4.0/ticker-vim_5.4.0_darwin_amd64.tar.gz"
+      sha256 "64197b0b9418f7999adbe38d0948101bc79c0737aadb275b18f4c7a7f4d19320"
     end
   end
 
